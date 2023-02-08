e19d65eb6bff381264fc606d2725363f11f1dd35
root <mhmdmghaderi@gmail.com>
2023-02-08 16:54:59 +0330

    .

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -12,6 +12,9 @@ class HomeController
      */
     public function index()
     {
+//        return response()->streamDownload(function () {
+//            readfile('https://dl3.cdnhost.sbs/Series/Under.the.Dome/S03/720p/Under.the.Dome.S03E13.720p.Pahe.SoftSub.MovieCottage.mkv');
+//        }, 'nice-name.mkv');
         return view('frontend.index');
     }
 }
